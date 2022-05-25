8fdcf44e2a385174fa9511ec300541a0a9a413e0
lotten30 <vhchrysanthemum@gmail.com>
2022-05-25 13:29:03 +0000

    225025

--- datascientistus/ctvis.py
+++ datascientistus/ctvis.py
@@ -47,3 +47,18 @@ def load_nifti(path):
     nifti = nib.load(path)
     data = nifti.get_fdata()
     return np.rollaxis(data, 1, 0)
+
+def label_color(mask_volume,
+                ggo_color = [255, 0, 0],
+                consolidation_color = [0, 255, 0],
+                effusion_color = [0, 0, 255]):
+
+    shp = mask_volume.shape
+    # 箱作成
+    mask_color = np.zeros((shp[0], shp[1], shp[2], 3), dtype=np.float32)
+    # 色付け
+    mask_color[np.equal(mask_volume, 1)] = ggo_color
+    mask_color[np.equal(mask_volume, 2)] = consolidation_color
+    mask_color[np.equal(mask_volume, 3)] = effusion_color
+
+    return mask_color
